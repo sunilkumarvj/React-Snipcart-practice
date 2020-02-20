248992417a4682d5fb1c94a742c2e20ed3e6bf48
V J <sunilkumara.v.j@accenture.com>
2020-02-20 13:26:41 +0530

    Updates

---
+++ precache-manifest.f8a1d934d6ca7e97b44df9bd4f0b5c3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88aad439a3b80411c5f71997183ca68d",
+    "url": "/React-Snipcart-practice/index.html"
+  },
+  {
+    "revision": "13e71421d3dac4b4d07f",
+    "url": "/React-Snipcart-practice/static/css/main.a0fca006.chunk.css"
+  },
+  {
+    "revision": "928044dc99f6a9d75482",
+    "url": "/React-Snipcart-practice/static/js/2.d5a6855d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Snipcart-practice/static/js/2.d5a6855d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13e71421d3dac4b4d07f",
+    "url": "/React-Snipcart-practice/static/js/main.a47d203a.chunk.js"
+  },
+  {
+    "revision": "d6900b2b0c7251822bf6",
+    "url": "/React-Snipcart-practice/static/js/runtime-main.1e876543.js"
+  }
+]);
